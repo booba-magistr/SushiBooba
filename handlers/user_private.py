5740a281e47a6d378a1ee9abbdf3249420fa0b68
booba-magistr <mainpo4ta1337@mail.ru>
2024-11-05 18:11:33 +0300

    add handler folder and connect router do dispatcher

---
+++ handlers/user_private.py
@@ -0,0 +1,14 @@
+from aiogram.filters import CommandStart, Command
+from aiogram import types, Router
+
+
+user_private_router = Router()
+
+
+@user_private_router.message(CommandStart())
+async def get_cmd(message: types.Message):
+    await message.answer('Бот начал работать')
+
+@user_private_router.message(Command('menu'))
+async def get_message(message: types.Message):
+    await message.answer('Ваше меню')
