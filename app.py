60c201a332897f51f848409bf04f9974714174db
booba-magistr <mainpo4ta1337@mail.ru>
2024-11-09 13:53:26 +0300

    add messages in handlers

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import os
 
 
 from aiogram import Bot, Dispatcher, types
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums.parse_mode import ParseMode
 
 from dotenv import find_dotenv, load_dotenv
 load_dotenv(find_dotenv())
@@ -12,7 +14,7 @@ from common.cmd_lst import private
 
 ALLOWED_UPDATES = ['message', 'edited_message']
 
-bot = Bot(token=os.getenv('TOKEN'))
+bot = Bot(token=os.getenv('TOKEN'), default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 
 dp = Dispatcher()
 dp.include_router(user_private_router)
--- handlers/user_private.py
+++ handlers/user_private.py
@@ -1,13 +1,32 @@
 from aiogram.filters import CommandStart, Command, or_f
 from aiogram import types, Router, F
+from aiogram.utils.formatting import as_list, as_marked_section, Bold
 
 
 user_private_router = Router()
 from keyboards import keyboard
 
 @user_private_router.message(CommandStart())
+@user_private_router.message(or_f(Command('about'), (F.text == 'О нас')))
 async def get_cmd(message: types.Message):
-    await message.answer('Бот начал работать', 
+    text = as_list(
+        as_marked_section(
+        Bold('Добро пожаловать в SushiBooba!!!'),
+        'Заказывайте суши онлайн и наслаждайтесь ими дома',
+        'Быстрая доставка',
+        'Только свежие и качественные продукты',
+        'Разнообразие меню - большой выбор роллов, суши',
+        marker='💥'
+    ),
+        as_marked_section(
+            Bold('Время работы'),
+            'Пн-Пт: 10:00-21:00',
+            'Сб-Вс: 10:00-20:00',
+            marker='⌚'
+        ),
+        sep='\n------------------------------\n'
+    )
+    await message.answer(text.as_html(),
                          reply_markup=keyboard.start_keyboard.as_markup(resize_keyboard=True))
 
 @user_private_router.message(or_f(Command('menu'), (F.text == 'Меню')))
@@ -16,12 +35,29 @@ async def get_message(message: types.Message):
 
 @user_private_router.message(or_f(Command('delivery'), (F.text == 'Способы получения')))
 async def get_delivery(message: types.Message):
-    await message.answer('Варианты доставки')
+    text = as_marked_section(
+        Bold('Способ получения'),
+        'Самовывоз',
+        'Доставка курьером',
+        marker='✅'
+    )
+    await message.answer(text.as_html())
 
 @user_private_router.message(or_f(Command('contact'), (F.text == 'Контакты')))
 async def get_contact(message: types.Message):
-    await message.answer('Контакты')
-
-@user_private_router.message(or_f(Command('about'), (F.text == 'О нас')))
-async def get_about(message: types.Message):
-    await message.answer('О нас') 
+    text = as_list(
+        as_marked_section(
+            Bold('Мы находимся'),
+            'Волгоград, пр. имени В.И. Ленина, 28',
+            'Волгоград, Рабоче-Крестьянская ул., 9 Б',
+            marker='🟢'
+        ),
+        as_marked_section(
+            Bold('Контакты'),
+            '8(995)423-27-47',
+            '8(909)048-07-32',
+            marker='📱'
+        ),
+        sep='\n------------------------------\n'
+    )
+    await message.answer(text.as_html())
