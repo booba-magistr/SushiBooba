2d09351ff89d093a41fddabf9f7a609c0aa27d23
booba-magistr <mainpo4ta1337@mail.ru>
2024-11-18 18:07:27 +0300

    add new models

--- tbot/database/models.py
+++ tbot/database/models.py
@@ -1,5 +1,5 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column
-from sqlalchemy import Text, String, Float, DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, declared_attr, mapped_column, relationship
+from sqlalchemy import Text, String, Float, DateTime, func, ForeignKey, Numeric
 
 
 class Base(DeclarativeBase):
@@ -13,9 +13,42 @@ class Base(DeclarativeBase):
         return cls.__name__.lower()
     
 
+class Banner(Base):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[int] = mapped_column(String(64), unique=True)
+    img: Mapped[str] = mapped_column(String(300), nullable=True)
+    description: Mapped[str] = mapped_column(String(500))
+    
+
+class Category(Base):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[int] = mapped_column(String(64), nullable=False)
+
+
 class Product(Base):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    category_id: Mapped[int] = mapped_column(ForeignKey('category.id', ondelete='CASCADE'))
     name: Mapped[str] = mapped_column(String(130), nullable=False)
     description: Mapped[str] = mapped_column(Text, nullable=False)
-    price: Mapped[float] = mapped_column(Float(asdecimal=True), nullable=False)
+    price: Mapped[float] = mapped_column(Numeric(6, 2))
     img: Mapped[str]
+
+    category: Mapped['Category'] = relationship(backref='product')
+
+
+class User(Base):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(unique=True)
+    first_name: Mapped[str] = mapped_column(String(64))
+    last_name: Mapped[str] = mapped_column(String(64), nullable=True)
+    phone: Mapped[str] = mapped_column(String(16), nullable=True)
+
+
+class Cart(Base):
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    user_id: Mapped[int] = mapped_column(ForeignKey('user.user_id', ondelete='CASCADE'))
+    product_id: Mapped[int] = mapped_column(ForeignKey('product.id', ondelete='CASCADE'))
+    quantity: Mapped[int]
+
+    user_id: Mapped['User'] = relationship(backref='cart')
+    product_id: Mapped['Product'] = relationship(backref='cart')
